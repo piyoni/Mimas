f3d40a43e61069010d8f7c9ace04dec6c39dbca5
piyoni <83449139+piyoni@users.noreply.github.com>
2023-10-17 12:17:15 +0200

    started adding basic functions for AST construction

--- Mimas/Parsing.fs
+++ Mimas/Parsing.fs
@@ -6,3 +6,10 @@ open PAST
 open ParseCom
 
 
+let ast1 (f: Tok->'a ) (Token(tk,cs):Token) =
+    ASTNode.ASTNode(f tk,(),cs) : ASTNode<'a,unit>
+let ast2 (f: Tok -> Tok ->'a ) (Token(tka,csa):Token) (Token(tkb,csb):Token) =
+    ASTNode.ASTNode(f tka tkb,(),codeSpanMerge csa csb) : ASTNode<'a,unit>
+let ast3 (f: Tok -> Tok -> Tok ->'a ) (Token(tka,csa):Token) (Token(tkb,csb):Token) (Token(tkc,csc):Token)=
+    ASTNode.ASTNode(f tka tkb tkc,(),codeSpanMerge (codeSpanMerge csa csb) csc) : ASTNode<'a,unit>
+
