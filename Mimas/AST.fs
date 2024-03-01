e4c6a1e76e0a8827fc4405989c52d8aefe52afb0
piyoni <83449139+piyoni@users.noreply.github.com>
2024-03-01 18:06:15 +0100

    beginning of redesign

---
+++ Mimas/AST.fs
@@ -0,0 +1,29 @@
+﻿module AST
+    open System.Numerics
+    type CodePos = {
+        col:int;
+        ln:int;
+    }
+
+    type ValType =
+        | Integer   of string
+        | Floating  of string
+        | String    of string
+        | Var       of string
+        | Ident     of string
+    
+
+    type Expr =
+        | Const of CodePos * CodePos * ValType
+        | App   of CodePos * CodePos * ValType * Expr List
+        | Lst   of CodePos * CodePos * Expr List
+        | Seq   of CodePos * CodePos * Expr * Expr
+        | Alt   of CodePos * CodePos * Expr * Expr
+        | Decls of CodePos * CodePos * Decl List
+    and Decl =
+        | Def of CodePos * CodePos * string * TypeHint * Expr
+        | Var of CodePos * CodePos * string * TypeHint * Expr
+    and TypeHint =
+        | NoHint
+        | Hint of Expr
+
--- Mimas/Program.fs
+++ Mimas/Program.fs
@@ -1,9 +1,8 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open FParsec
-open Parser
 let teststr = "\"hello\" 12343 _as As\n234897493978263 28954734.2"
 
-printfn "%A\n" (parseFile "programs/test.txt")
+printfn "%s\n\n" "test"
 
 //printfn "%s\n\n" (graphNodeMake 0 "test" "hello" [])
 //printfn "%s" (graphEdgesAdd 2 ["n4";"n5";"n120"])
